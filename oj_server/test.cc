91113ae92d6a825e9edf80a26b9cc0e96b35724f
WangYouCoder <1684025215@qq.com>
2024-07-16 14:23:39 +0800

    在线OJ项目_负载均衡

--- oj_server/test.cc
+++ oj_server/test.cc
@@ -37,59 +37,59 @@
 // }
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
 
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
         
-    }
-};
+//     }
+// };
 
-#ifndef COMPILE
-#include "header.hpp"
-#endif
+// #ifndef COMPILE
+// #include "header.hpp"
+// #endif
 
-void Test1()
-{
-    vector<int> v{2,7,11,15};
+// void Test1()
+// {
+//     vector<int> v{2,7,11,15};
 
-    auto ret = Solution().twoSum(v, 9);
+//     auto ret = Solution().twoSum(v, 9);
 
-    if(ret.size() == 2 && ret[0] == 0 && ret[1] == 1)
-    {
-        cout << "通过测试用例一" <<endl;
-    }
-    else
-    {
-        cout << "未通过测试用例一" <<endl;
-    }
-}
+//     if(ret.size() == 2 && ret[0] == 0 && ret[1] == 1)
+//     {
+//         cout << "通过测试用例一" <<endl;
+//     }
+//     else
+//     {
+//         cout << "未通过测试用例一" <<endl;
+//     }
+// }
 
-void Test2()
-{
-    vector<int> v{3,2,4};
+// void Test2()
+// {
+//     vector<int> v{3,2,4};
 
-    auto ret = Solution().twoSum(v, 6);
+//     auto ret = Solution().twoSum(v, 6);
 
-    if(ret.size() == 2 && ret[0] == 1 && ret[1] == 2)
-    {
-        cout << "通过测试用例一" <<endl;
-    }
-    else
-    {
-        cout << "未通过测试用例一" <<endl;
-    }
-}
+//     if(ret.size() == 2 && ret[0] == 1 && ret[1] == 2)
+//     {
+//         cout << "通过测试用例一" <<endl;
+//     }
+//     else
+//     {
+//         cout << "未通过测试用例一" <<endl;
+//     }
+// }
 
-int main()
-{
-    Test1();
-    Test2();
+// int main()
+// {
+//     Test1();
+//     Test2();
 
-    return 0;
-}
+//     return 0;
+// }
